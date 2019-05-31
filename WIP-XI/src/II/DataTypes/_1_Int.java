2033a4e522fdf1b1a99b9e2fd9226b953bd70b1f
Mhamaii <50911441+Mhamaii@users.noreply.github.com>
2019-05-31 08:43:48 +0700

    Add project

---
+++ WIP-XI/src/II/DataTypes/_1_Int.java
@@ -0,0 +1,21 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package II.DataTypes;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Int {
+    public static void main(String[] args) {
+        int x = 900;
+        int y = 20;
+        int z = 100;
+        
+        int sum = (x-y)*z;
+        
+        System.out.println("sum : " + sum);
+    }
+}
